c48d65503dcf9db70a308621d747f512e1ecae2e
Monish Prasanna S <143189813+monishprasanna@users.noreply.github.com>
2026-05-14 23:57:16 +0530

    Change base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Using repository path base optimized for GitHub Pages deployment.
-  base: '/monish_port/',
+  base: '/portfolioo/',
 });
